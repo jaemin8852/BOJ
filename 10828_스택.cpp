a56c9131921e9d21dbdf9b9f5011ff6c9138d7a4
jaemin8852 <jaemin8852@naver.com>
2018-07-26 01:47:59 +0900

    Create 10828_스택.cpp

---
+++ 10828_스택.cpp
@@ -0,0 +1,50 @@
+//10828_스택
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+
+	stack<int> stk;
+	int n;
+	int data;
+	string str;
+
+	cin >> n;
+
+	for (int i = 0; i < n; ++i) {
+		cin >> str;
+
+		if (str == "push") {		//data 받고 push
+			cin >> data;
+			stk.push(data);
+		}
+
+		else if (str == "pop") {				//empty check하고 비어 있으면 -1,
+			if (stk.empty()) cout << "-1\n";	//값이 있으면 top 값 출력 후, pop
+			else {
+				cout << stk.top() << "\n";
+				stk.pop();
+			}
+		}
+
+		else if (str == "size") {			//stack size 출력
+			cout << stk.size() << "\n";
+		}
+
+		else if (str == "empty") {			//stack empty 출력 (비어 있으면 1, 아니면 0)
+			cout << stk.empty() << "\n";
+		}
+
+		else if (str == "top") {			//empty check하고 비어 있으면 -1,
+			if (stk.empty()) cout << "-1\n";	//값이 있으면 top 값 출력, pop은 X
+			else {
+				cout << stk.top() << "\n";
+			}
+		}
+	}
+
+	return 0;
+}
